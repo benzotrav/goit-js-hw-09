a696a45292b1a24523820ae27cd6b9c67ca057b6
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-21 23:49:51 +0300

    task 1 = done + delete task 2 (bugs with flickr)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startButton: document.querySelector('button[data-start]'), 
+    stopButton: document.querySelector('button[data-stop]'), 
+    body: document.querySelector('body'), 
+};
 
+refs.startButton.addEventListener('click', startChangingColor);
+refs.stopButton.addEventListener('click', stopChangingColor); 
+
+let intervalId = null;  
+
+function startChangingColor() { 
+    refs.startButton.setAttribute('disabled', true); 
+    refs.stopButton.removeAttribute('disabled'); 
+    intervalId = setInterval(changeColor, 1000); 
+}
+
+function stopChangingColor() { 
+    refs.startButton.removeAttribute('disabled'); 
+    refs.stopButton.setAttribute('disabled', true); 
+    clearInterval(intervalId); 
+}
+
+function changeColor() { 
+    refs.body.style.backgroundColor = getRandomHexColor(); 
+}
+
+function getRandomHexColor() { 
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
+}
