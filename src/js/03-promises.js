476c352d6eef84e58aaf325c19825ca515fa2c98
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-22 22:17:55 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form'); 
+form.addEventListener('submit', onFormSubmit); 
+
+const data = {}; 
+
+function onFormSubmit(event) { 
+  event.preventDefault(); 
+  dataGrabber(event.currentTarget); 
+  promiseRunner(); 
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) { 
+  const shouldResolve = Math.random() > 0.3; 
+ 
+  return new Promise((resolve, reject) => { 
+    setTimeout(() => { 
+      if (shouldResolve) { 
+        resolve({ position, delay }); 
+      } else { 
+        reject({ position, delay }); 
+      }
+    }, delay); 
+  });
+}
+
+function promiseRunner() { 
+  const { delay: firstDelay, step, amount } = data; 
+  let delay = firstDelay; 
+  
+  for (let i = 1; i <= amount; i+=1) { 
+    createPromise(i, delay).then(successPromise).catch(failurePromise); 
+    delay += step; 
   }
+}
+
+function dataGrabber(form) { 
+  [...form.elements].forEach(({ name, value }) => (data[name] = Number(value))); 
+}
+
+
+function successPromise({ position, delay }) { 
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`); 
+} 
+
+function failurePromise({ position, delay }) { 
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`); 
 }
